3e2fad6eb4208c74a470e6a1d92d2c522dcc4939
Rita <ritakialex@gmail.com>
2021-11-22 21:42:11 +0200

    commit dbconnect-gitignore

---
+++ lib/dbconnect.php
@@ -0,0 +1,21 @@
+<?php
+$host='localhost';
+//να οριστεί η βάση
+$db = 'THE_DB';             
+require_once "config_local.php";
+
+$user=$DB_USER;
+$pass=$DB_PASS;
+
+
+if(gethostname()=='users.iee.ihu.gr') {
+	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2016/it164852/mysql/run/mysql.sock');
+} else {
+        $mysqli = new mysqli($host, $user, $pass, $db);
+}
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . 
+    $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}?>
+
