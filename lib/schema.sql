07ee1afacce1dfcfba499827c5d0ce99ebf8fc84
Rita <ritakialex@gmail.com>
2021-12-23 03:47:06 +0200

    changes on schema, new files users and game
    changes on schema - added url on cards,
    new files users and game

--- lib/schema.sql
+++ lib/schema.sql
@@ -90,56 +90,58 @@ DROP TABLE IF EXISTS `cards_for_moutzouris`;
 
 CREATE TABLE `cards_for_moutzouris` (
   `c_id` int(11) NOT NULL AUTO_INCREMENT,
-  `c_name` enum('ace','two','three','four','five','six','seven','eight','nine','ten','jack','queen','king') NOT NULL,
+  `c_name` enum('ace','two','three','four','five','six','seven','eight','nine','ten','jack','queen','king','back') NOT NULL,
   `c_value` int(11) NOT NULL,
-  `c_suit` enum('spades','clubs','hearts','diamonds') NOT NULL,
+  `c_suit` enum('spades','clubs','hearts','diamonds','back') NOT NULL,
+  `c_url` varchar(150) NOT NULL,
   PRIMARY KEY (`c_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8 MAX_ROWS=52;
 
 /*Data for the table `cards_for_moutzouris` */
 
-insert  into `cards_for_moutzouris`(`c_id`,`c_name`,`c_value`,`c_suit`) values 
-(1,'ace',1,'clubs'),
-(2,'two',2,'clubs'),
-(3,'three',3,'clubs'),
-(4,'four',4,'clubs'),
-(5,'five',5,'clubs'),
-(6,'six',6,'clubs'),
-(7,'seven',7,'clubs'),
-(8,'eight',8,'clubs'),
-(9,'nine',9,'clubs'),
-(10,'ten',10,'clubs'),
-(14,'ace',1,'diamonds'),
-(15,'two',2,'diamonds'),
-(16,'three',3,'diamonds'),
-(17,'four',4,'diamonds'),
-(18,'five',5,'diamonds'),
-(19,'six',6,'diamonds'),
-(20,'seven',7,'diamonds'),
-(21,'eight',8,'diamonds'),
-(22,'nine',9,'diamonds'),
-(23,'ten',10,'diamonds'),
-(27,'ace',1,'hearts'),
-(28,'two',2,'hearts'),
-(29,'three',3,'hearts'),
-(30,'four',4,'hearts'),
-(31,'five',5,'hearts'),
-(32,'six',6,'hearts'),
-(33,'seven',7,'hearts'),
-(34,'eight',8,'hearts'),
-(35,'nine',9,'hearts'),
-(36,'ten',10,'hearts'),
-(40,'ace',1,'spades'),
-(41,'two',2,'spades'),
-(42,'three',3,'spades'),
-(43,'four',4,'spades'),
-(44,'five',5,'spades'),
-(45,'six',6,'spades'),
-(46,'seven',7,'spades'),
-(47,'eight',8,'spades'),
-(48,'nine',9,'spades'),
-(49,'ten',10,'spades'),
-(52,'king',13,'spades');
+insert  into `cards_for_moutzouris`(`c_id`,`c_name`,`c_value`,`c_suit`,`c_url`) values 
+(1,'ace',1,'clubs','https://upload.wikimedia.org/wikipedia/commons/3/36/Playing_card_club_A.svg\''),
+(2,'two',2,'clubs','https://upload.wikimedia.org/wikipedia/commons/f/f5/Playing_card_club_2.svg\''),
+(3,'three',3,'clubs','https://upload.wikimedia.org/wikipedia/commons/6/6b/Playing_card_club_3.svg\'\r\n'),
+(4,'four',4,'clubs','https://upload.wikimedia.org/wikipedia/commons/3/3d/Playing_card_club_4.svg\''),
+(5,'five',5,'clubs','https://upload.wikimedia.org/wikipedia/commons/5/50/Playing_card_club_5.svg\''),
+(6,'six',6,'clubs','https://upload.wikimedia.org/wikipedia/commons/a/a0/Playing_card_club_6.svg\''),
+(7,'seven',7,'clubs','https://upload.wikimedia.org/wikipedia/commons/4/4b/Playing_card_club_7.svg\''),
+(8,'eight',8,'clubs','https://upload.wikimedia.org/wikipedia/commons/e/eb/Playing_card_club_8.svg\''),
+(9,'nine',9,'clubs','https://upload.wikimedia.org/wikipedia/commons/2/27/Playing_card_club_9.svg\''),
+(10,'ten',10,'clubs','https://upload.wikimedia.org/wikipedia/commons/3/3e/Playing_card_club_10.svg\''),
+(14,'ace',1,'diamonds','https://upload.wikimedia.org/wikipedia/commons/d/d3/Playing_card_diamond_A.svg\''),
+(15,'two',2,'diamonds','https://upload.wikimedia.org/wikipedia/commons/5/59/Playing_card_diamond_2.svg\''),
+(16,'three',3,'diamonds','https://upload.wikimedia.org/wikipedia/commons/8/82/Playing_card_diamond_3.svg\''),
+(17,'four',4,'diamonds','https://upload.wikimedia.org/wikipedia/commons/2/20/Playing_card_diamond_4.svg\''),
+(18,'five',5,'diamonds','https://upload.wikimedia.org/wikipedia/commons/f/fd/Playing_card_diamond_5.svg\''),
+(19,'six',6,'diamonds','https://upload.wikimedia.org/wikipedia/commons/8/80/Playing_card_diamond_6.svg\''),
+(20,'seven',7,'diamonds','https://upload.wikimedia.org/wikipedia/commons/e/e6/Playing_card_diamond_7.svg\''),
+(21,'eight',8,'diamonds','https://upload.wikimedia.org/wikipedia/commons/7/78/Playing_card_diamond_8.svg\''),
+(22,'nine',9,'diamonds','https://upload.wikimedia.org/wikipedia/commons/9/9e/Playing_card_diamond_9.svg\''),
+(23,'ten',10,'diamonds','https://upload.wikimedia.org/wikipedia/commons/3/34/Playing_card_diamond_10.svg\''),
+(27,'ace',1,'hearts','https://upload.wikimedia.org/wikipedia/commons/5/57/Playing_card_heart_A.svg\''),
+(28,'two',2,'hearts','https://upload.wikimedia.org/wikipedia/commons/d/d5/Playing_card_heart_2.svg\''),
+(29,'three',3,'hearts','https://upload.wikimedia.org/wikipedia/commons/b/b6/Playing_card_heart_3.svg\''),
+(30,'four',4,'hearts','https://upload.wikimedia.org/wikipedia/commons/a/a2/Playing_card_heart_4.svg\''),
+(31,'five',5,'hearts','https://upload.wikimedia.org/wikipedia/commons/5/52/Playing_card_heart_5.svg\''),
+(32,'six',6,'hearts','https://upload.wikimedia.org/wikipedia/commons/c/cd/Playing_card_heart_6.svg\''),
+(33,'seven',7,'hearts','https://upload.wikimedia.org/wikipedia/commons/9/94/Playing_card_heart_7.svg\''),
+(34,'eight',8,'hearts','https://upload.wikimedia.org/wikipedia/commons/5/50/Playing_card_heart_8.svg\''),
+(35,'nine',9,'hearts','https://upload.wikimedia.org/wikipedia/commons/5/50/Playing_card_heart_9.svg\''),
+(36,'ten',10,'hearts','https://upload.wikimedia.org/wikipedia/commons/9/98/Playing_card_heart_10.svg\''),
+(40,'ace',1,'spades','https://upload.wikimedia.org/wikipedia/commons/2/25/Playing_card_spade_A.svg\''),
+(41,'two',2,'spades','https://upload.wikimedia.org/wikipedia/commons/f/f2/Playing_card_spade_2.svg\''),
+(42,'three',3,'spades','https://upload.wikimedia.org/wikipedia/commons/5/52/Playing_card_spade_3.svg\''),
+(43,'four',4,'spades','https://upload.wikimedia.org/wikipedia/commons/2/2c/Playing_card_spade_4.svg\''),
+(44,'five',5,'spades','https://upload.wikimedia.org/wikipedia/commons/9/94/Playing_card_spade_5.svg\''),
+(45,'six',6,'spades','https://upload.wikimedia.org/wikipedia/commons/d/d2/Playing_card_spade_6.svg\''),
+(46,'seven',7,'spades','https://upload.wikimedia.org/wikipedia/commons/6/66/Playing_card_spade_7.svg\''),
+(47,'eight',8,'spades','https://upload.wikimedia.org/wikipedia/commons/2/21/Playing_card_spade_8.svg\''),
+(48,'nine',9,'spades','https://upload.wikimedia.org/wikipedia/commons/e/e0/Playing_card_spade_9.svg\''),
+(49,'ten',10,'spades','https://upload.wikimedia.org/wikipedia/commons/8/87/Playing_card_spade_10.svg\''),
+(52,'king',13,'spades','https://upload.wikimedia.org/wikipedia/commons/9/9'),
+(53,'back',14,'back','https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Card_back_01.svg/703px-Card_back_01.svg.png');
 
 /*Table structure for table `deck1` */
 
@@ -181,27 +183,39 @@ DROP TABLE IF EXISTS `game_status`;
 
 CREATE TABLE `game_status` (
   `status` enum('not active','initialized','started','ended','aborded') NOT NULL DEFAULT 'not active',
-  `p_turn` enum('player 1','player 2') DEFAULT NULL,
-  `last_change` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+  `p_turn` enum('p1','p2') DEFAULT NULL,
+  `result` enum('p1','p2','draw') DEFAULT NULL,
+  `last_change` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `game_status` */
 
-/*Table structure for table `player` */
+/*Table structure for table `players` */
 
-DROP TABLE IF EXISTS `player`;
+DROP TABLE IF EXISTS `players`;
 
-CREATE TABLE `player` (
-  `p_id` int(11) NOT NULL,
+CREATE TABLE `players` (
+  `p_id` enum('p1','p2') NOT NULL,
   `p_username` varchar(20) DEFAULT NULL,
-  `p_no_cardsInHand` int(21) DEFAULT NULL,
-  `p_score` int(11) DEFAULT NULL,
-  `p_turn` enum('yes','no') DEFAULT NULL,
-  `p_token` varchar(25) DEFAULT NULL,
+  `token` varchar(100) DEFAULT NULL,
+  `p_last_action` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`p_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `player` */
+/*Data for the table `players` */
+
+/* Trigger structure for table `game_status` */
+
+DELIMITER $$
+
+/*!50003 DROP TRIGGER*//*!50032 IF EXISTS */ /*!50003 `game_status_update` */$$
+
+/*!50003 CREATE */ /*!50017 DEFINER = 'root'@'localhost' */ /*!50003 TRIGGER `game_status_update` BEFORE UPDATE ON `game_status` FOR EACH ROW BEGIN
+SET NEW.last_change = NOW();
+END */$$
+
+
+DELIMITER ;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
---
+++ lib/users.php
@@ -0,0 +1,87 @@
+<?php
+
+function show_users() {
+	global $mysqli;
+	$sql = 'select p_username,p_id from players';
+	$st = $mysqli->prepare($sql);
+	$st->execute();
+	$res = $st->get_result();
+	header('Content-type: application/json');
+	print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
+}
+function show_user($b) {
+	global $mysqli;
+	$sql = 'select p_username,p_id from players where p_id=?';
+	$st = $mysqli->prepare($sql);
+	$st->bind_param('s',$b);
+	$st->execute();
+	$res = $st->get_result();
+	header('Content-type: application/json');
+	print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
+}
+
+function set_user($b,$input) {
+	//print_r($input);
+	if(!isset($input['p_username']) || $input['p_username']=='') {
+		header("HTTP/1.1 400 Bad Request");
+		print json_encode(['errormesg'=>"No username given."]);
+		exit;
+	}
+	$p_username=$input['p_username'];
+	global $mysqli;
+	$sql = 'select count(*) as c from players where p_id=? and p_username is not null';
+	$st = $mysqli->prepare($sql);
+	$st->bind_param('s',$b);
+	$st->execute();
+	$res = $st->get_result();
+	$r = $res->fetch_all(MYSQLI_ASSOC);
+	if($r[0]['c']>0) {
+		header("HTTP/1.1 400 Bad Request");
+		print json_encode(['errormesg'=>"Player $b is already set. Please select another color."]);
+		exit;
+	}
+	$sql = 'update players set p_username=?, token=md5(CONCAT( ?, NOW()))  where p_id=?';
+	$st2 = $mysqli->prepare($sql);
+	$st2->bind_param('sss',$p_username,$p_username,$b);
+	$st2->execute();
+
+
+	
+	update_game_status();
+	$sql = 'select * from players where p_id=?';
+	$st = $mysqli->prepare($sql);
+	$st->bind_param('s',$b);
+	$st->execute();
+	$res = $st->get_result();
+	header('Content-type: application/json');
+	print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
+	
+	
+}
+
+function handle_user($method, $b,$input) {
+	if($method=='GET') {
+		show_user($b);
+	} else if($method=='PUT') {
+        set_user($b,$input);
+    }
+}
+function current_color($token) {
+	
+	global $mysqli;
+	if($token==null) {return(null);}
+	$sql = 'select * from players where token=?';
+	$st = $mysqli->prepare($sql);
+	$st->bind_param('s',$token);
+	$st->execute();
+	$res = $st->get_result();
+	if($row=$res->fetch_assoc()) {
+		return($row['p_id']);
+	}
+	return(null);
+}
+
+
+
+
+?>
