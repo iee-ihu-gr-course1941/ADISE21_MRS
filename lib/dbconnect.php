0f10a3bd7800fc0188841f93421dfcb4412e166b
Rita <ritakialex@gmail.com>
2022-01-06 02:24:35 +0200

    to-test-on-users

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -2,15 +2,19 @@
 $host='localhost';
 $db = 'adise21_mrs';             
 require_once "config_local.php";
+$user="root";
+$pass="zxcvasdf!";
 
-$user=$DB_USER;
-$pass=$DB_PASS;
+/*$user=$DB_USER;
+$pass=$DB_PASS;*/
 
 
 if(gethostname()=='users.iee.ihu.gr') {
 	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2016/it164852/mysql/run/mysql.sock');
+    echo "Connected successfully on users";
 } else {
         $mysqli = new mysqli($host, $user, $pass, $db);
+        echo "Connected successfully on localhost db";
 }
 
 if ($mysqli->connect_errno) {
